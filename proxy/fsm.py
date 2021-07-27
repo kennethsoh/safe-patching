788f12d71b8a2bc1dfa16c2525f0f5137a94812e
Kenneth Soh <34814795+kennethsoh@users.noreply.github.com>
2021-07-27 09:55:44 +0800

    Create fsm.py

---
+++ proxy/fsm.py
@@ -0,0 +1,41 @@
+'''
+File Name       : fsm.py
+Author          : Kenneth Soh
+Date created    : 30 June 2021
+'''
+
+import os
+import sys
+import time
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+class Monitor(FileSystemEventHandler):
+
+    def on_modified(self, event):
+        filename, ext = os.path.splitext(event.src_path)
+        if (ext == ".json"):
+            print(f"python3 proxy.py {filename}")
+            print(f"python3 proxy.py {ext}")
+            print(f"python3 proxy.py {event.src_path}")
+            os.system(f'python3 proxy.py {event.src_path}')
+            pass
+        else:
+            pass
+
+if __name__ == "__main__":
+    src_path = sys.argv[1] if len(sys.argv) > 1 else 'files/'
+    
+    event_handler=Monitor()
+    observer = Observer()
+    observer.schedule(event_handler, path=src_path, recursive=True)
+    print("Monitoring started")
+    print(src_path)
+    observer.start()
+    try:
+        while(True):
+           time.sleep(1)
+           
+    except KeyboardInterrupt:
+            observer.stop()
+            observer.join()
